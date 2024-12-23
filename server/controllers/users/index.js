5be5ea7498f46d10202c91c062862ce4fbd99f12
saadhn4 <saadcfi4@gmail.com>
2024-12-23 08:52:16 +0530

    done till put

--- server/controllers/users/index.js
+++ server/controllers/users/index.js
@@ -7,6 +7,10 @@ router.post("/register", async (req, res) => {
   try {
     let userData = req.body;
     console.log(userData);
+    let userCheck = await userModel.find({ username: userData.username });
+    if (userCheck) {
+      return res.status(404).json({ msg: "User already registered!!" });
+    }
     await userModel.create(userData);
     res.status(200).json({ msg: "User Registered! 💻📱" });
   } catch (error) {
@@ -15,9 +19,10 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.get("/getuser", async (req, res) => {
+router.get("/getuser/:email", async (req, res) => {
   try {
-    let user = await userModel.find({ username: "saad.hn" });
+    let userParams = req.params.email;
+    let user = await userModel.find({ email: userParams });
     console.log(user);
     res.status(200).json(user);
   } catch (error) {
@@ -34,29 +39,24 @@ router.get("/getallusers", async (req, res) => {
     console.log(error);
     res.status(500).json({ msg: error });
   }
+});
 
-  router.put("/update/:email", async (req, res) => {
-    try {
-      let userParams = req.params.email;
-      let userUpdate = req.body;
-      console.log("Params:", req.params);
-      console.log("Body:", req.body);
+/*
+In updateOne, or in deleteOne, you should only take unique values. (username,email,phoneNumber,id)
 
-      await userModel.updateOne({ email: userParams }, { $set: userUpdate });
-      res.status(200).json({ msg: "Updated!" });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ msg: error });
-    }
-  });
-  router.delete("/deleteall", (req, res) => {
-    try {
-      res.status(200).json({ msg: "Deleting many users" });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ msg: error });
-    }
-  });
+*/
+
+router.put("/put/:suhail", async (req, res) => {
+  try {
+    let userParams = req.params.suhail;
+    let userUpdate = req.body;
+
+    await userModel.updateOne({ username: userParams }, { $set: userUpdate });
+    res.status(200).json({ msg: "Updated!" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: error });
+  }
 });
 
 export default router;
